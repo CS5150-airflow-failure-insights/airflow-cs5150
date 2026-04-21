cb2d25b4c0aa6d6061f6429cea689d99ea75e10a
katieperlitz <kap256@cornell.edu>
2026-04-21 13:35:28 -0400

    added Sruti's share_airflow_env file and additional enviroment variable file for breeze supabase setup

---
+++ share_airflow_env.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+export AIRFLOW_HOME="/Users/srutigudapati/Desktop/airflow-cs5150/.airflow_home"
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://postgres.ybbzonuwfmzlacfoyefh:airflow_user5150@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
+export PATH="/Users/srutigudapati/Desktop/airflow-cs5150/.venv/bin:$PATH"
